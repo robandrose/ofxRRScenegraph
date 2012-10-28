384410c2acd4ae9bf6cee0004ed125a54b8a4083
Matthias Rohrbach <m.rohrbach@robandrose.ch>
2012-10-28 13:41:14 +0100

    added key events to basic controller, and draw for debug (or timeline component)

--- src/controllers/basics/BasicController.cpp
+++ src/controllers/basics/BasicController.cpp
@@ -10,24 +10,34 @@
 #include "BasicController.h"
 
 BasicController::BasicController() {
-	
-	
 	myEventArgs.target=this;
-	
 	
 	ofAddListener(ofEvents().setup, this, &BasicController::_setup);
     ofAddListener(ofEvents().update, this, &BasicController::_update);
+	ofAddListener(ofEvents().draw, this, &BasicController::_draw);
 	ofAddListener(ofEvents().exit, this, &BasicController::_exit);
 	
+	ofAddListener(ofEvents().keyPressed, this, &BasicController::_keyPressed);
+	ofAddListener(ofEvents().keyReleased, this, &BasicController::_keyReleased);
 	
 }
 
 BasicController::~BasicController() {
 	ofRemoveListener(ofEvents().setup, this, &BasicController::_setup);
     ofRemoveListener(ofEvents().update, this, &BasicController::_update);
+	ofRemoveListener(ofEvents().draw, this, &BasicController::_draw);
 	ofRemoveListener(ofEvents().exit, this, &BasicController::_exit);
+	
+	ofRemoveListener(ofEvents().keyPressed, this, &BasicController::_keyPressed);
+	ofRemoveListener(ofEvents().keyReleased, this, &BasicController::_keyReleased);
+	
 }
 
 void BasicController::_setup(ofEventArgs &e){setup();}
 void BasicController::_update(ofEventArgs &e){update();}
+void BasicController::_draw(ofEventArgs &e){draw();}
 void BasicController::_exit(ofEventArgs &e){exit();}
+
+void BasicController::_keyPressed(ofKeyEventArgs &e){keyPressed(e);}
+void BasicController::_keyReleased(ofKeyEventArgs &e){keyReleased(e);}
+
--- src/controllers/basics/BasicController.h
+++ src/controllers/basics/BasicController.h
@@ -16,15 +16,24 @@ class BasicController {
 public:
 	BasicController();
 	virtual ~BasicController();
+
 	
 protected:
 	void _setup(ofEventArgs &e);
 	void _update(ofEventArgs &e);
+	void _draw(ofEventArgs &e);
 	void _exit(ofEventArgs &e);
+	void _keyPressed(ofKeyEventArgs &e);
+	void _keyReleased(ofKeyEventArgs &e);
+	
+		
 	
 	virtual void setup(){};
 	virtual void update(){};
+	virtual void draw(){};
 	virtual void exit(){};
+	virtual void keyPressed(ofKeyEventArgs &e){};
+	virtual void keyReleased(ofKeyEventArgs &e){};
 	
 	BasicControllerEvent myEventArgs;
 	
