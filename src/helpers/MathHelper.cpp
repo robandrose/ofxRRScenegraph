52826f2752579bc725dfda2b8d822f414a5e8ac4
Patrick <p.meister@robandrose.ch>
2012-11-23 13:49:00 +0100

    added minInt, maxInt, minFloat, maxFloat to MathHelper. returns the respective smaller/larger value.

--- src/helpers/MathHelper.cpp
+++ src/helpers/MathHelper.cpp
@@ -19,3 +19,23 @@ bool MathHelper::isAnlgeBetween(float _angle, float _startAngle, float _endAngle
 float MathHelper::cleanAngle(float _dirtyAngle) {
 	return fmod(360 + fmod(_dirtyAngle,360),360);
 }
+
+int MathHelper::minInt(int a, int b) {
+	if (b < a) return b;
+	return a;
+}
+
+float MathHelper::minFloat(float a, float b) {
+	if (b<a) return b;
+	return a;
+}
+
+int MathHelper::maxInt(int a, int b) {
+	if (b > a) return b;
+	return a;
+}
+
+float MathHelper::maxFloat(float a, float b) {
+	if (b > a) return b;
+	return a;
+}
--- src/helpers/MathHelper.h
+++ src/helpers/MathHelper.h
@@ -13,4 +13,8 @@ class MathHelper  {
 public:
 	static bool isAnlgeBetween(float _angle, float _startAngle, float _endAngle);
 	static float cleanAngle(float _dirtyAngle);
+	static int minInt(int a, int b);	
+	static float minFloat(float a, float b);
+	static int maxInt(int a, int b);	
+	static float maxFloat(float a, float b);
 };
