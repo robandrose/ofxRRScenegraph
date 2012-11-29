c4e94210e1c542a2345c604eb57d7138a1ee386d
Patrick <p.meister@robandrose.ch>
2012-11-29 12:18:08 +0100

    removed unnecessary texture-allocations and other stuff. refactored, re-organized code and variable names. nice code formatting.

--- src/views/elements/TextField.cpp
+++ src/views/elements/TextField.cpp
@@ -13,14 +13,12 @@ const int TextField::ALIGN_RIGHT=PANGO_ALIGN_RIGHT;
 const int TextField::ALIGN_CENTER=PANGO_ALIGN_CENTER;
 
 
-TextField::TextField(){
-	
-	pango = new ofxPango();
-	context=NULL;
-	layout=NULL;
-	fd=NULL;
-	
-	changed=false;
+TextField::TextField() {
+	pango	= new ofxPango();
+	context	= NULL;
+	layout	= NULL;
+	fd		= NULL;
+	changed	= false;
 		
 	setFontSize(15);
 	setFontName("Graphik");
@@ -31,128 +29,148 @@ TextField::TextField(){
 	setAntialiasType(CAIRO_ANTIALIAS_DEFAULT);
 }
 
-TextField::~TextField(){
-	
+
+void TextField::setup() {
+	layout = pango->createLayout(width, height);
+	layout->setWidth(width);
+	layout->fill(0, 0, 0, 0);
 }
 
-void TextField::setup(){
-	
-}
 
-void TextField::update(){	
+void TextField::update() {	
 	if(changed){
-		// TODO: fix this bug. if rendered only once, text is scrambled sometimes.
-		renderText();
 		renderText();
 	}
 }
 
-void TextField::setText(string _text){
-	if(_text==mytext)return;
-	mytext=_text;
 
-	changed=true;
+void TextField::_draw() {
+	glBlendFunc(GL_ONE, GL_ONE_MINUS_SRC_ALPHA); // To avoid ugly dark eges in alpha blending
+	textImage.draw(0, 0);
 }
 
-string TextField::getText(){
-	return mytext;	
+
+void TextField::updateFontDescription() {
+	setFontDescription(fontName+" "+ofToString(fontSize));	
 }
 
-void TextField::setFontName(string _fontname) {
-	fontname=_fontname;
-	updateFontDescription();
-}
 
-void TextField::setFontSize(float _fontsize){
-	fontsize=_fontsize;
-	updateFontDescription();
-}
-
-void TextField::updateFontDescription(){
-	setFontDescription(fontname+" "+ofToString(fontsize));	
-}
-
-void TextField::setFontDescription(string _fontdescription){
+void TextField::setFontDescription(string _fontDescription) {
 	if(fd!=NULL){
 		delete fd;
 	}	
-	fontdescription=_fontdescription;	
+	fontDescription=_fontDescription;	
 	fd = new ofxPCPangoFontDescription();
-	fd->createFromString(fontdescription);
+	fd->createFromString(fontDescription);
 	changed=true;
 }
 
 
+void TextField::setText(string _text) {
+	if(_text == textContent) return;
+	textContent = _text;
+	changed=true;
+}
 
-void TextField::setSize(float _width, float _height){
+
+void TextField::setFontName(string _fontName) {
+	if(_fontName == fontName) return;
+	fontName = _fontName;
+	updateFontDescription();
+}
+
+
+void TextField::setFontSize(float _fontSize) {
+	if(_fontSize == fontSize) return;
+	fontSize = _fontSize;
+	updateFontDescription();
+}
+
+
+void TextField::setSize(float _width, float _height) {
 	BasicScreenObject::setSize(_width, _height);
 	if (layout != NULL) delete layout;
-	layout = pango->createLayout(width, height);
-	layout->setWidth(width); 
-	layout->fill(0, 0, 0, 0);
-	text_image.clear();
-	text_image.allocate(layout->getWidth(), layout->getHeight(), OF_IMAGE_COLOR_ALPHA);	
-	changed=true;
-}
-
-void TextField::setTextAlign(int _textalign){
-	align=_textalign;
-	changed=true;
-}
-
-void TextField::setLineSpacing(int _linespacing){
-	linespacing=_linespacing;
-	changed=true;
-}
-
-void TextField::setIndent(float _indent){
-	indent=_indent;
-	changed=true;
-}
-
-void TextField::setTabs(vector<int> _tabs){
-	tabs=_tabs;
-	changed=true;
-}
-
-void TextField::setColor(float _r, float _g, float _b){
-	BasicScreenObject::setColor(_r,_g,_b);
+	layout = pango->createLayout(width, height);	
 	changed=true;
 }
 
 
-void TextField::setColor(ofColor _c){
-	BasicScreenObject::setColor(_c.r,_c.g,_c.b);
-	changed=true;
+void TextField::setTextAlign(int _textAlign) {
+	if(_textAlign == textAlign) return;
+	textAlign	= _textAlign;
+	changed		= true;
 }
 
-void TextField::setAntialiasType(cairo_antialias_t _type) {
-	antialias_type = _type;
+
+void TextField::setLineSpacing(int _lineSpacing) {
+	if(_lineSpacing == lineSpacing) return;
+	lineSpacing	= _lineSpacing;
+	changed		= true;
+}
+
+
+void TextField::setIndent(float _indent) {
+	if(_indent == indent) return;
+	indent	= _indent;
+	changed	= true;
+}
+
+
+void TextField::setTabs(vector<int> _tabs) {
+	tabs	= _tabs;
 	changed = true;
 }
 
-ofPoint TextField::getTextBounds(){
+
+void TextField::setColor(float _r, float _g, float _b) {
+	BasicScreenObject::setColor(_r,_g,_b);
+	changed = true;
+}
+
+
+void TextField::setColor(ofColor _c) {
+	BasicScreenObject::setColor(_c.r,_c.g,_c.b);
+	changed = true;
+}
+
+
+void TextField::setAntialiasType(cairo_antialias_t _type) {
+	if(_type == antialiasType) return;
+	antialiasType	= _type;
+	changed			= true;
+}
+
+
+ofPoint TextField::getTextBounds() {
 	if (changed) {
-		renderText();
 		renderText();
 	}
 	return bounds;
 }
 
-void TextField::renderText(){
+
+ofxPCPangoLayout* TextField::getLayout() {
+	return layout;
+}
+
+
+string TextField::getText() {
+	return textContent;	
+}
+
+
+void TextField::renderText() {
 
 	layout->context->clear();
-	layout->setWidth(width);
-	layout->setFontDescription(*fd, antialias_type);
-	layout->setTextColor(color.r/255.0f,color.g/255.0f,color.b/255.0f,1);
-	layout->setSpacing(linespacing);	
+	layout->setFontDescription(*fd, antialiasType);
+	layout->setTextColor(color.r/255.0f, color.g/255.0f, color.b/255.0f, 1);
+	layout->setSpacing(lineSpacing);	
 	layout->setIndent(indent);
-	layout->setMarkup(mytext);
+	layout->setMarkup(textContent);
 	layout->setTabs(tabs);
-	layout->setPangoAlign(align);
+	layout->setPangoAlign(textAlign);
 	layout->show();
 
-	text_image.setFromPixels(layout->context->getSurface()->getPixels(), text_image.width, text_image.height, OF_IMAGE_COLOR_ALPHA, true);
 	bounds=layout->getPixelSize();	
 	
 	changed=false;
@@ -161,13 +179,5 @@ void TextField::renderText(){
 		setSize(width, bounds.y);
 	}
 	
-}
-
-void TextField::_draw(){
-	glBlendFunc(GL_ONE, GL_ONE_MINUS_SRC_ALPHA); // To avoid ugly dark eges in alpha blending
-	text_image.draw(0, 0);
-}
-
-ofxPCPangoLayout* TextField::getLayout(){
-	return layout;
+	textImage.setFromPixels(layout->context->getSurface()->getPixels(), width, height, OF_IMAGE_COLOR_ALPHA, true);
 }
--- src/views/elements/TextField.h
+++ src/views/elements/TextField.h
@@ -15,24 +15,25 @@
 class TextField : public BasicScreenObject{
 public:
 	TextField();
-	virtual ~TextField();
-	void setup();
-	void update();
-	void _draw();
+	virtual ~TextField() {};
 	
-	void setText(string _text);
-	string getText();
+	void	setup();
+	void	update();
+	void	_draw();
 	
-	void setFontName(string _fontname);
-	void setFontSize(float _fontsize);	
-	void setFontDescription(string _fontdescription);
+	void	setText(string _text);
+	string	getText();
 	
-	void setSize(float _width, float _height);
-	void setTextAlign(int _textalign);
-	void setLineSpacing(int _linespacing);
-	float getLineSpacing(){return linespacing;};
-	void setAntialiasType(cairo_antialias_t _type); 
-	cairo_antialias_t getAntialiasType() {return antialias_type;};
+	void	setFontName(string _fontname);
+	void	setFontSize(float _fontsize);	
+	void	setFontDescription(string _fontdescription);
+	
+	void	setSize(float _width, float _height);
+	void	setTextAlign(int _textalign);
+	void	setLineSpacing(int _linespacing);
+	float	getLineSpacing(){return lineSpacing;};
+	void	setAntialiasType(cairo_antialias_t _type); 
+	cairo_antialias_t getAntialiasType() {return antialiasType;};
 	
 	void setIndent(float _indent);
 	void setTabs(vector<int> _tabs);
@@ -40,36 +41,36 @@ public:
 	virtual void setColor(float _r, float _g, float _b);
 	virtual void setColor(ofColor _c);
 	
-	ofPoint getTextBounds();	
-	ofxPCPangoLayout* getLayout();
+	ofPoint				getTextBounds();	
+	ofxPCPangoLayout*	getLayout();
 	
 	static const int ALIGN_CENTER;
 	static const int ALIGN_LEFT;
 	static const int ALIGN_RIGHT;
 	
-private:
+protected:
 	
-	ofxPango*			pango;
-	ofxPCContext*		context;
-	ofxPCPangoLayout*	layout;
-	ofxPCPangoFontDescription* fd;
+	ofxPango*					pango;
+	ofxPCContext*				context;
+	ofxPCPangoLayout*			layout;
+	ofxPCPangoFontDescription*	fd;
 	
-	ofImage		text_image;	
+	ofImage		textImage;	
 	
-	string		mytext;	
-	string		fontname;
-	float		fontsize;
-	string		fontdescription;
-	int			align;
+	string		textContent;	
+	string		fontName;
+	float		fontSize;
+	string		fontDescription;
+	int			textAlign;
 	bool		justify;
-	cairo_antialias_t	antialias_type;
+	cairo_antialias_t	antialiasType;
 	
-	int linespacing;
-	float indent;
-	vector<int> tabs;
-	ofPoint bounds;
+	int			lineSpacing;
+	float		indent;
+	vector<int>	tabs;
+	ofPoint		bounds;
 	
-	bool changed;
+	bool		changed;
 	
 	void updateFontDescription();
 	void renderText();	
